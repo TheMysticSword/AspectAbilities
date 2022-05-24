8822f0adcb2890a334639665ae44c2e0ef86a3ae
David Galiev <galiev.david@yandex.ru>
2022-05-24 20:57:25 +0500

    Delete BaseBuff.cs

--- Buffs/BaseBuff.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace AspectAbilities.Buffs
-{
-    public abstract class BaseBuff : MysticsRisky2Utils.BaseAssetTypes.BaseBuff
-    {
-        public override string TokenPrefix => AspectAbilitiesPlugin.TokenPrefix;
-    }
-}
